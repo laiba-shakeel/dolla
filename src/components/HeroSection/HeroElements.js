67c3aa5096b83c6a8b691734acec4094db6c36cb
Laiba <lili.shaqil@gmail.com>
2022-11-02 14:40:58 +0500

    initializing the data file , for using multiple time`s

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -10,10 +10,20 @@ padding: 0 30px;
 height: 800px;
 position: relative;
 z-index: 1;
-
-// Add :before styles
-
+ 
+:before{
+   content: '';
+   position: absolute;
+   top: 0;
+   right:0;
+   left:0;
+   bottom: 0;
+   background: linear-gradient(180deg, rgba(0,0,0,0.2) 0% , rgba(0,0,0,0.6) 100%),
+   linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+   z-index: 2;
+}
 `
+;
   export const HeroBg = styled.div`
   position: absolute;
   top: 0;
@@ -73,7 +83,7 @@ z-index: 1;
  margin-top: 32px;
  display:flex;
  flex-direction: column;
- align-items: cemter;
+ align-items: center;
  `
 
  export const ArrowForward = styled(MdArrowForward)`
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about' , 
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'prenium Bnak',
+    heading: 'Unlimited Transections with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transections without getting charged any fees, ',
+    buttonLabel: 'Get Started',
+    imgStart: flase ,
+    img: require('../../images/svg-1.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
